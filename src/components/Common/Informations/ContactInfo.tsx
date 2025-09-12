692e2314cb3c16671b64953c191419ecd9c0d49e
RidaKhaoua <ridakhaoua04@gmail.com>
2025-09-12 01:09:55 +0100

    update

--- src/components/Common/Informations/ContactInfo.tsx
+++ src/components/Common/Informations/ContactInfo.tsx
@@ -6,6 +6,7 @@ import Phone from "@assets/svg/phone.svg?react";
 import Email from "@assets/svg/envlope.svg?react";
 
 function ContactInfo({email, phone}: TContactInfo) {
+  //ddjhdjh
   return (
     <ul>
         <List title={phone}  icon={<Phone title='phone'/>}/>
